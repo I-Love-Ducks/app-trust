a7b991a446e986b40c5756c8b92e3b5b8c228b20
Arhaan <saulatarhaan@gmail.com>
2025-06-06 17:16:24 -0500

    chart will work. not styled yet. i have bigger problems tho. gotta change position absolute

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const burger = document.querySelector('.burger_container');
 const menu = document.querySelector('.menu');
 const vertLine = document.querySelector('.vertical-line');
 const month_year_stat = document.querySelector('.month_and_year_stats_container');
+const menu_container = document.querySelector('.menu_container')
+
 
 burger.addEventListener('click', function() {
     menu.classList.toggle('active');
+    menu_container.classList.toggle('active');
     vertLine.classList.toggle('active');
     month_year_stat.classList.toggle('menuOpened');
 })
